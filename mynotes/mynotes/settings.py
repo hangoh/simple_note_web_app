d738089d338b89fb4a3b62a0b005dd7b23b50f18
hangoh <74915663+hangoh@users.noreply.github.com>
2021-02-13 23:39:49 +0900

    Update settings.py

--- mynotes/mynotes/settings.py
+++ mynotes/mynotes/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'mynotes',
         'USER': 'postgres',
-        'PASSWORD': 'GOHyuhan123',
+        'PASSWORD': 'xxxxxxxxx',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
